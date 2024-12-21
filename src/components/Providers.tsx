071ec47064807cf75978e67369f65a44071c8c68
chihab <chihab.mg.me@gmail.com>
2024-12-21 20:03:08 +0100

    added progress bar

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { CartProvider } from '@/context/CartContext'
+import { AppProgressBar } from 'next-nprogress-bar'
 import React, { ReactNode } from 'react'
 
 const Providers = ({children}:{children:ReactNode}) => {
   return (
     <>
+    <AppProgressBar options={{showSpinner:false}} />
     <CartProvider>
         {children}
     </CartProvider>
