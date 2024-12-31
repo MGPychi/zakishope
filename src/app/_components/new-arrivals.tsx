fbc899e9ab812048608d1d1a75241443982c0da6
chihab <chihab.mg.me@gmail.com>
2024-12-31 19:17:30 +0100

    improved the home page

--- src/app/_components/new-arrivals.tsx
+++ src/app/_components/new-arrivals.tsx
@@ -2,34 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Card, CardContent } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
+import { getLatestProducts } from '../data/products-data'
 
-export function NewArrivals() {
-  const products = [
-    {
-      id: 1,
-      name: "Smart Watch Pro",
-      price: "12,900 DA",
-      image: "/placeholder.svg?height=200&width=200"
-    },
-    {
-      id: 2,
-      name: "Wireless Earbuds",
-      price: "8,900 DA",
-      image: "/placeholder.svg?height=200&width=200"
-    },
-    {
-      id: 3,
-      name: "4K Android TV Box",
-      price: "11,900 DA",
-      image: "/placeholder.svg?height=200&width=200"
-    },
-    {
-      id: 4,
-      name: "Security Camera",
-      price: "9,900 DA",
-      image: "/placeholder.svg?height=200&width=200"
-    }
-  ]
+export async function NewArrivals() {
+  const latestProducts = await getLatestProducts()
 
   return (
     <section className="py-8">
@@ -40,20 +16,25 @@ export function NewArrivals() {
         </Link>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {products.map((product) => (
+        {latestProducts.map((product) => (
           <Card key={product.id} className="group">
             <CardContent className="p-4">
               <div className="relative aspect-square mb-3">
                 <Badge className="absolute top-2 right-2 z-10">Nouveau</Badge>
                 <Image
-                  src={product.image}
+                  src={product.images[0].url}
                   alt={product.name}
                   fill
                   className="object-cover rounded-md group-hover:scale-105 transition-transform"
                 />
               </div>
-              <h3 className="font-medium">{product.name}</h3>
-              <p className="text-primary">{product.price}</p>
+              <div className='min-h-[60px]'>
+
+                <h3 className="font-medium">
+                {product.name.length > 50 ? `${product.name.substring(0, 50)}...` : product.name}
+                </h3>
+              </div>
+              <p className="text-primary">{product.price}DA</p>
             </CardContent>
           </Card>
         ))}
