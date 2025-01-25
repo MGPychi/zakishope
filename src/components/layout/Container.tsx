e70c397ce31511e7e0fc6faab20de1bd1c44d2bf
chihab <chihab.mg.me@gmail.com>
2025-01-25 21:51:35 +0100

    decreased the padding on mobile

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="mx-auto px-4 sm:px-0 max-w-screen-2xl ">{children}</div>
+    <div className="mx-auto px-1 sm:px-0 max-w-screen-2xl ">{children}</div>
   );
 };
 
