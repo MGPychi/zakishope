2e355b58a3ca76dd5093511aa06a72b36c920d76
chihab <chihab.mg.me@gmail.com>
2025-02-07 18:03:54 +0100

    added an animation to the header cart

--- src/components/layout/site-header/site-header-cart.tsx
+++ src/components/layout/site-header/site-header-cart.tsx
@@ -2,19 +2,42 @@
 import { Button } from "@/components/ui/button";
 import { useCart } from "@/hooks/useCart";
 import { ShoppingBag } from "lucide-react";
-import React from "react";
+import * as motion from "motion/react-m";
+import { useEffect, useState } from "react";
+
 const SiteHeaderCart = () => {
-    const {items} = useCart()
+  const { items } = useCart();
+  const [itemCount, setItemCount] = useState(items.length);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (items.length !== itemCount) {
+      setAnimate(true);
+      setItemCount(items.length);
+    }
+  }, [items.length, itemCount]);
+
   return (
     <Button
       variant="ghost"
       size="icon"
       className="relative text-gray-600 hover:text-tahat-800"
     >
-      <ShoppingBag className="h-5 w-5" />
-      <span className="absolute bg-primary -top-1 -right-1 h-4 w-4 rounded-full bg-tahat-800 text-xs text-white flex items-center justify-center">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <ShoppingBag className="h-5 w-5" />
+      </motion.div>
+      <motion.span
+        className="absolute bg-primary -top-1 -right-1 h-4 w-4 rounded-full bg-tahat-800 text-xs text-white flex items-center justify-center"
+        initial={{ scale: 0 }}
+        animate={animate ? { scale: [1.2, 1] } : { scale: 1 }}
+        transition={{ type: "spring", stiffness: 300 }}
+        onAnimationComplete={() => setAnimate(false)}
+      >
         {items.length}
-      </span>
+      </motion.span>
     </Button>
   );
 };
